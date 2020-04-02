0260a7797f96de820b424958aceda9df69ee5ad5
lucas024 <lucas.a.perry98@gmail.com>
2020-04-02 23:53:46 +0100

    adicionado iterador de mês e notificações de validade

--- index.js
+++ index.js
@@ -4,23 +4,22 @@ var itensMenu= ["verDispensa"];
 
 var elementosDispensa=["Leite","Àgua", "Sumo", "Arroz", "Massa", "Azeite", "Carne", "Peixe", "Cereais", "Ovos", "Aveia"]
 var valoresDispensa=[6,12,2,2,1,2,3,2,2,12,3]
-var validadeDispensa=[2,24,5,18,18,2,1,1,12,2,24]
+var validadeDispensa=[3,24,5,18,18,6,3,2,12,3,24]
 var inDispensa = false;
-
-
+var mesesPassados = 4;
+var anosPassados = 2020;
 function start() {
     setDate();
     setDispensa();
   }
 window.onload = start;
+
 function setDate(){
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
 
-    today = dd + '/' + mm + '/' + yyyy;
-    document.getElementById("date").innerHTML=today;
+    var data = dd + '/' + String(mesesPassados).padStart(2, '0') + '/' + String(anosPassados);
+    document.getElementById("date").innerHTML=data;
 }
 
 
@@ -44,6 +43,7 @@ function setDispensaHandler(i){
     h32.innerHTML=this.valoresDispensa[i] + " Unid";  
     var p = document.createElement("p");
     p.style.marginTop="20px";
+    p.id=elemento+"mes";
     p.innerHTML=this.validadeDispensa[i].toString() + " Meses";
     var img = document.createElement("img");
     img.src="minus.png";
@@ -75,6 +75,7 @@ function removeElement(e){
             verDispensa()
         }
     }
+    
 }
 
 function verMenu(){
@@ -102,7 +103,7 @@ function verDispensa(){
         h31.innerHTML=elemento+" : ";
         var h32 = document.createElement("h3");
         var nrElementos = valoresDispensa[i];
-        h32.innerHTML=nrElementos;
+        h32.innerHTML=nrElementos + " Unid";
         var p = document.createElement("p");
         p.style.marginTop="20px";
         p.innerHTML=this.validadeDispensa[i] + " Meses";
@@ -132,7 +133,7 @@ function addElement(){
         setDispensaHandler(elementosDispensa.length - 1)
     }
     else{
-        alert("Wrong Item")
+        alert("Tenta outra vez")
     }
   
     if(inDispensa){
@@ -142,5 +143,81 @@ function addElement(){
     }
     document.getElementById("addBox").style.visibility="hidden"
     document.getElementById("backdrop").style.visibility="hidden"
-    
 }
+function addMes(){
+    for(i=0; i<validadeDispensa.length; i++){
+        atual = validadeDispensa[i]
+        if(atual > 0){
+            reduzido = atual - 1;
+            validadeDispensa[i] = reduzido;
+            document.getElementById(elementosDispensa[i]+"mes").innerHTML=reduzido+" Meses";
+        }
+    }
+    
+    mesesPassados+=1;
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    if(mesesPassados==13){
+        anosPassados+=1;
+        mesesPassados=0;
+    }
+    var data = dd + '/' + String(mesesPassados).padStart(2, '0') + '/' + String(anosPassados);
+    document.getElementById("date").innerHTML=data;
+    if(inDispensa){
+        var rem = document.getElementById("verDispensa");
+        rem.remove()
+        verDispensa()
+    }
+    verificaENotifica()
+}
+
+//"ATENÇÃO: de acordo com os seus hábitos alimentares, o produto"+ elementosDispensa[i]+ " acabará este mês!"
+
+function verificaENotifica(){
+    console.log(validadeDispensa)
+    for(i = 0; i<elementosDispensa.length; i++){
+        if(validadeDispensa[i]==1){
+            document.getElementById("backdropMovel").style.visibility="visible"
+            var newNotificacao = document.createElement("div");
+            newNotificacao.style.width="80%"
+            newNotificacao.style.height="110px"
+            newNotificacao.style.background="#1e3c72"
+            newNotificacao.style.border="4px solid black"
+            newNotificacao.style.borderRadius="6px"
+            newNotificacao.style.margin="auto"
+            newNotificacao.style.marginTop="10px"
+            newNotificacao.style.zIndex="27"
+            newNotificacao.className="notificacao"
+            var newP = document.createElement("p")
+            newP.style.color="white"
+            newP.style.fontSize="20px"
+            newP.innerHTML="ATENÇÃO: " + "O produto "+ elementosDispensa[i]+ " acaba a sua validade este mês"
+            newNotificacao.append(newP)
+            var zonaButtons = document.createElement("div");
+            zonaButtons.style.display="flex"
+            zonaButtons.style.justifyContent="space-evenly"
+            zonaButtons.style.marginTop="10px"
+            var buttonOk = document.createElement("button")
+            var buttonReceitas = document.createElement("button")
+            buttonOk.style.backgroundColor="#44c767"
+            buttonOk.style.borderRadius="28px"
+            buttonOk.innerHTML="OK"
+            buttonReceitas.style.backgroundColor="#44c767"
+            buttonReceitas.style.borderRadius="28px"
+            buttonReceitas.innerHTML="Receitas com "+elementosDispensa[i].toLowerCase()
+            zonaButtons.append(buttonOk)
+            zonaButtons.append(buttonReceitas)
+            newNotificacao.append(zonaButtons)
+            document.getElementById("zonaNotificacoes").append(newNotificacao)
+        }
+    }
+}
+
+function removeNotificationHandler(){
+    var listNot = document.getElementsByClassName("notificacao")
+    listNot[0].remove()
+    if(listNot.length==0){
+        document.getElementById("backdropMovel").style.visibility="hidden"
+    }
+}
+
